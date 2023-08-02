6a6e492dd1daf45b3016fc22b6790bde37f62226
Bashir Rashid <karche475b@gmail.com>
2023-08-02 10:30:00 +0300

    solution for the TIP_CALCULATOR activity

--- challenge.js
+++ challenge.js
@@ -1,2 +1,21 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let bill = 75
+
+let tip
+
+if(bill >= 50 && bill <=300)
+{
+     tip = (bill * 15)/100 
+     console.log(`The bill was ${bill}, and the tip was ${tip}, so`)
+}
+else if(bill > 300)
+{
+     tip = (bill * 20)/100
+     console.log(`The bill was ${bill}, and the tip was ${tip}, so`)
+}
+
+let total_value = bill + tip
+console.log(`The total amount is ${total_value}`)
